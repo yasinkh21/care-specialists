5f3f745faa93898b651446973be939ff283eb59a
yasin <yasinkhalifaa29@gmail.come>
2025-05-11 17:18:03 +0300

    COMMIT

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-const navbar = decument.queryselector('.navbar')
+const navbar = decument.queryselector('.navbar');
+console.log(43434)
 
 window.addEventListener("scroll",()=>{
+    console.log(2323232)
     if (scrollY > 62 ){
         navbar.classList.add('fixedd')
     }else {
