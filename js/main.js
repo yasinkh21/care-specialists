8bb2d4fe4f706bb6d17d0cd91f0d6ffe48232d8b
Wael Khalifa <wnkhalifa99@gmail.com>
2025-05-11 17:09:14 +0300

    minor change ˆ

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
-const navbar = decument.queryselector('.navbar')
+const navbar = document.getElementsByClassName('navbar')[0];
 
-window.addEventListener("scroll",()=>{
-    if (scrollY > 62 ){
-        navbar.classList.add('fixedd')
-    }else {
-        navbar.classList.remove('fixedd')
+window.addEventListener("scroll", () => {
+    if (scrollY > 62) {
+        navbar.classList.add('fixedd');
+    } else {
+        navbar.classList.remove('fixedd');
     }
-
 
 })
 
